dac82ad77f5de9f13271f153fa8fa1e859018bd2
Minh Le <qminh279@gmail.com>
2023-08-06 15:52:11 -0400

    setup linter and fix current errors

--- src/content.js
+++ src/content.js
@@ -1,15 +1,19 @@
-const main = () => {
-  const callback = (mutationList, observer) => {
-    const target = document.getElementsByClassName("results-out-of");
-    if (target.length > 0) {
-      observer.disconnect();
-      manipulate();
-    }
-  };
-
-  const config = { childList: true, subtree: true };
-  const observer = new MutationObserver(callback);
-  observer.observe(document.body, config);
+const manipulate = () => {
+	console.log('TODO');
 };
 
-window.addEventListener("load", main);
+const main = () => {
+	const callback = (mutationList, observer) => {
+		const target = document.getElementsByClassName('results-out-of');
+		if (target.length > 0) {
+			observer.disconnect();
+			manipulate();
+		}
+	};
+
+	const config = { childList: true, subtree: true };
+	const observer = new MutationObserver(callback);
+	observer.observe(document.body, config);
+};
+
+window.addEventListener('load', main);
