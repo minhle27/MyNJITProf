cf87df451e4e02fef2ed91d514e1671b508d9241
Minh Le <qminh279@gmail.com>
2023-08-06 15:47:13 -0400

    update content script

---
+++ src/content.js
@@ -0,0 +1,15 @@
+const main = () => {
+  const callback = (mutationList, observer) => {
+    const target = document.getElementsByClassName("results-out-of");
+    if (target.length > 0) {
+      observer.disconnect();
+      manipulate();
+    }
+  };
+
+  const config = { childList: true, subtree: true };
+  const observer = new MutationObserver(callback);
+  observer.observe(document.body, config);
+};
+
+window.addEventListener("load", main);
